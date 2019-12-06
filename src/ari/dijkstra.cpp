4f5bcdfeef6799f0436cab6272cea36905ae33f2
kmzn128 <54118781+kmzn128@users.noreply.github.com>
2019-12-06 12:42:46 -0800

    Create dijkstra.cpp

---
+++ src/ari/dijkstra.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for (int i = 0; i < (int)(n); i++)
+#define REP2(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define RREP2(i, a, b) for (int i = (a); i > (int)(b); i--)
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef long long ll;
+
+int Main()
+{
+    
+    return 0;
+}
+
+int main(void){
+    Main();
+    return 0;
+}
