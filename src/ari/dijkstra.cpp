9a616d5b7899bb4e69b07c4eb53019323c042b30
kmzn128 <54118781+kmzn128@users.noreply.github.com>
2019-12-08 05:31:46 -0800

    Revert "created abc147"
    This reverts commit a19f42406cb9d2527bd07a762937f15b75607e40.

--- src/ari/dijkstra.cpp
+++ src/ari/dijkstra.cpp
@@ -2,22 +2,18 @@
 using namespace std;
 #define REP(i, n) for (int i = 0; i < (int)(n); i++)
 #define REP2(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define RREP2(i, a, b) for (int i = (a); i > (int)(b); i--)
 typedef vector<int> VI;
 typedef vector<VI> VVI;
+typedef long long ll;
 
-string Main(int a, int b, int c)
+int Main()
 {
-    if(a+b+c >= 22) {
-        return "bust";
-    }
-    else {
-        return "win";
-    }
+    
+    return 0;
 }
 
 int main(void){
-    int A, B, C;
-    cin >> A >> B >> C;
-    cout << Main(A, B, C) << endl;
+    Main();
     return 0;
 }
