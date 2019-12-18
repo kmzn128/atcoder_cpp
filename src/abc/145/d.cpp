31eacff834b38d0662a18d6a2d492a7755e9b59c
kmzn128 <54118781+kmzn128@users.noreply.github.com>
2019-12-18 13:47:34 -0800

    Create d.cpp

---
+++ src/abc/145/d.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for (int i = 0; i < (int)(n); i++)
+#define REP2(i, a, b) for (int i = (a); i < (int)(b); i++)
+typedef long long ll;
+typedef vector<ll> VL;
+typedef vector<VL> VVL;
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res+=a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res-=a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res*=a;
+  }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const {
+    return pow(mod-2);
+  }
+  mint& operator/=(const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/(const mint a) const {
+    mint res(*this);
+    return res/=a;
+  }
+};
+
+struct combination {
+  vector<mint> fact, ifact;
+  combination(int n):fact(n+1),ifact(n+1) {
+    assert(n < mod);
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) fact[i] = fact[i-1]*i;
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) ifact[i-1] = ifact[i]*i;
+  }
+  mint operator()(int n, int k) {
+    if (k < 0 || k > n) return 0;
+    return fact[n]*ifact[k]*ifact[n-k];
+  }
+};
+ 
+int Main(int X, int Y)
+{
+    if((X+Y)%3) return 0;
+    int n, m;
+    n = (2*X - Y) / 3;
+    m = (2*Y - X) / 3;
+    if(n < 0 || m < 0) return 0;
+    combination c(n+m);
+    return c(m+n, n).x;
+
+}
+ 
+int main(void){
+    int X, Y;
+    cin >> X >> Y;
+    cout << Main(X, Y) << endl;
+    return 0;
+}
